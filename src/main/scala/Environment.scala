fb7779ab9b87e0e06c7c7e0207b4a2d7e2320b58
Stephen Judkins <stephen@aboutus.org>
2009-10-15 17:28:19 -0700

    Removed unnecessary config load.

--- src/main/scala/Environment.scala
+++ src/main/scala/Environment.scala
@@ -3,7 +3,6 @@ import net.lag.configgy.Configgy
 import net.lag.logging.Logger
 
 object Environment {
-  val config = Configgy.configure("s3q.config")
   var environment:Environment = new Environment
   def env = { environment }
 }
